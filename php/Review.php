570b5c5085674c8a8bfcb186997a799508feb979
MaloNier <Hisamaru020@yahoo.co.jp>
2019-12-29 18:42:07 +0900

    再読み込みボタン追加

--- php/Review.php
+++ php/Review.php
@@ -12,6 +12,17 @@ class Review {
 	private $text;
 
 	/**
+	 * コンストラクタ
+	 *
+	 * @param int $subjectId 科目ID
+	 * @param string $review レビュー文
+	 */
+	function __construct($subjectId, $review) {
+		$this->subjectId = $subjectId;
+		$this->review = $review;
+	}
+
+	/**
 	 * レビュー文を取得する
 	 *
 	 * @return string レビュー文
@@ -29,3 +40,16 @@ class Review {
 		$this->text = $text;
 	}
 }
+
+$sbReview = [
+	new Review(0, 'temp00'),
+	new Review(1, 'temp01'),
+	new Review(2, 'temp02'),
+	new Review(3, 'temp03'),
+	new Review(4, 'temp04'),
+	new Review(5, 'temp05'),
+	new Review(6, 'temp06'),
+	new Review(7, 'temp07'),
+	new Review(8, 'temp08'),
+	new Review(9, 'temp09')
+];
